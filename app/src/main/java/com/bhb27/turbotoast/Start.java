3ecd2b1303e4898b11f4d3dc6c26695aca4ed95b
Felipe Leon <fglfgl27@gmail.com>
2017-05-29 10:54:12 -0300

    Add a start Activity
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

---
+++ app/src/main/java/com/bhb27/turbotoast/Start.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) Felipe de Leon <fglfgl27@gmail.com>
+ *
+ * This file is part of iSu.
+ *
+ * iSu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * iSu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with iSu.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.bhb27.turbotoast;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.bhb27.turbotoast.Main;
+
+public class Start extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.startActivity(new Intent(this, Main.class).setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT));
+        finish();
+    }
+
+}
